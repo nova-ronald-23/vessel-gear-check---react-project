8544aab043d0d258cb5e3ff8661dfa9bb932acb7
Mìʀяoя_Mᴀɴí <manikandantn.in@gmail.com>
2024-03-06 13:09:13 +0530

    last update mani

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,80 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios'; 
-import Log from './log'; // Correct import statement for log.js
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import VesselProfile from './components/VesselProfile';
+import VesselProfileNext from './components/VesselProfileNext';
+import Sidebar from './components/Sidebar';
 
 function App() {
-  const [students, setStudents] = useState([]);
-  const [formData, setFormData] = useState({
-    name: '',
-    age: '',
-  });
-
-  useEffect(() => {
-    // Fetch data from your API
-    axios.get('https://localhost:7231/api/Student')
-      .then(response => setStudents(response.data))
-      .catch(error => console.error('Error fetching data:', error));
-  }, []);
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // Send data to the server
-    axios.post('https://localhost:7231/api/Student/Creatingstudent', formData)
-      .then(response => {
-        console.log('Data submitted successfully:', response.data);
-        // You might want to update the local state with the new data
-        setStudents([...students, response.data]);
-        setFormData({ name: '', age: '' });
-      })
-      .catch(error => console.error('Error submitting data:', error));
-  };
-
   return (
-    <div>
-      <h1><center>Student List</center></h1>
-      <table className='table'>
-        <thead>
-          <tr>
-            <th>SNo</th>
-            <th>Name</th>
-            <th>Age</th>
-          </tr>
-        </thead>
-        <tbody>
-          {students.map((student, index) => (
-            <tr key={student.id}>
-              <td>{index + 1}</td>
-              <td>{student.name}</td>
-              <td>{student.age}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-
-      <form onSubmit={handleSubmit} className="form">
-        <label>
-          Name:
-          <input type="text" name="name" value={formData.name} onChange={handleInputChange} />
-        </label>
-        <br />
-        <label>
-          Age:
-          <input type="text" name="age" value={formData.age} onChange={handleInputChange} />
-        </label>
-        <br />
-        <button type="submit">Submit</button>
-      </form>
-
-
-    </div>
+    <Router>
+      <Routes>
+      <Route path="/" element={<VesselProfile />} />
+      <Route path="/VesselProfileNext" element={<VesselProfileNext />} />
+      <Route path="/Sidebar" element={<Sidebar />} />
+       
+      
+        {/* Other routes */}
+      </Routes>
+    </Router>
   );
 }
-//nova change
+
 export default App;
---
+++ frontend/src/components/VesselProfileNext.js
@@ -0,0 +1,84 @@
+import React from 'react';
+
+import { useNavigate } from 'react-router-dom';
+import './styles.css'; // Import CSS file for styling
+
+
+const VesselProfileNext = () => {
+ 
+    const navigate = useNavigate();
+
+    const Back = () => {
+        // Add your login logic here
+        //alert(`Are you sure to logout`);
+        // Redirect to the dashboard after successful login
+        navigate('/');
+      };
+ 
+
+
+
+
+
+  return (
+    <div className="container">
+      <div className="sidebar">
+      <div className="sheader">
+      <div className="captain-logo"></div>
+      <div className="captain-text">Captain</div>
+      </div>
+
+      <div className="sidebar-options">
+          <button className="option">
+            <img src={require("../components/images/opn1.png")} alt="Option 1" /> 
+            <span>Roles</span>
+          </button>
+          <button className="option">
+            <img src={require("../components/images/opn2.png")} alt="Option 2" /> 
+            <span>Vessel Profiile</span>
+          </button>
+          <button className="option">
+            <img src={require("../components/images/opn3.png")} alt="Option 3" /> 
+            <span>Checklist</span>
+          </button>
+          <button className="option">
+            <img src={require("../components/images/opn4.png")} alt="Option 4" /> 
+            <span>Visualization</span>
+          </button>
+          <button className="option">
+            <img src={require("../components/images/opn5.png")} alt="Option 5" /> 
+            <span>Settings</span>
+          </button>
+            {/* Add more options as needed */}
+
+      </div>
+        <button className="sign-out-button">
+          <img src={require("../components/images/sout.png")} alt="Sign Out" />
+          <span>Sign Out</span>
+          </button>
+
+      </div>
+
+      <div className="main-content">
+        <div className="box">
+        <div className="header">Vessel Profile</div>
+          <div className="text-fields">
+              <input type="text" placeholder="Field 1" />
+              <input type="text" placeholder="Field 2" />
+              <input type="text" placeholder="Field 3" />
+              <input type="text" placeholder="Field 4" />
+              <input type="text" placeholder="Field 5" />
+              <input type="text" placeholder="Field 6" />
+            </div>
+            <div className="button-container">
+            <button className="button1" onClick={() => Back()}>Back</button>
+           
+            <button className="button2">Submit</button>
+          
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default VesselProfileNext;
