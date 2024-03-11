845140f997db7a78eacdbc501644877febf1f144
nova ronald s <69787583+nova-ronald-23@users.noreply.github.com>
2024-03-11 15:33:11 +0530

    nova commits api

---
+++ vessel-gear-check-api/vessel-gear-check-api/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using vessel_gear_check_api.Models;
+
+namespace vessel_gear_check_api.Data
+{
+    public class ApplicationDbContext:DbContext
+
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Checklist> Checklists { get; set; }
+    }
+}
