cf12f1cbdcc57a6edea0851d5e3114a2e5cb348c
Sarmesh365 <140476002+Sarmesh365@users.noreply.github.com>
2024-03-15 12:29:20 +0530

    sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './App.css'; // Assuming you have a CSS file for styling
 //import { Set } from './components/Set';
 //import RolesScreen from './components/Roles';
-import { Personal } from './components/Personal';
+//import { Personal } from './components/Personal';
+import Sidebar from './components/Sidebar';
 
 
 
 function App() {
   return (
     <div>
-    <Personal/>
+    <Sidebar/>
+    
        
     </div>
   );
--- frontend/src/components/Personal.js
+++ frontend/src/components/Personal.js
@@ -4,24 +4,33 @@ import './Personal.css'
 export const Personal = () => {
   return (
     <div className="container">
-    <div className="main-content">
-      <div className="white-box">
-      <div className="captain-logo"></div>
+        <div className="main-content">
+            <div className="white-box">
+                <div className="captain-logo"></div>
               <input type="text" placeholder="Enter email ID" />
               
-              <input type="text" placeholder="Enter Current Position" />
-          <div className="button-container">
-            <button className="button1" >Exit</button>
-           
-            <button className="button2" >Update</button>
-          
-          </div>
-        
-            
-            
-      </div>
+              <input type="text" placeholder="Enter your Name" />
+              <div className="dropdown">
+                   <select>
+                           <option value="">Select Your Position</option>
+                           <option value="option1">Captain</option>
+                           <option value="option2">Bridge</option>
+                           <option value="option3">Engine</option>
+                           <option value="option4">Logistics</option>
+                           <option value="option5">Safety</option>
+                           <option value="option6">Hospitality</option>                       
+                   </select>
+              </div>
+              <div className="button-container">
+                   <button className="button1" >Exit</button>
+                  
+                   <button className="button2" >Update</button>
+              
+              </div>   
+                       
+            </div>
+        </div>
     </div>
-  </div>
 
   )
 }
