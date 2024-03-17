9c10f5e589bdc80bf2a9b28bc310f221a3414168
nova ronald s <69787583+nova-ronald-23@users.noreply.github.com>
2024-03-17 18:07:13 +0530

    nova commits after all frontend combainted without checklist

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,59 +1,17 @@
-// index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store from './components/Checklist/store';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import CreateChecklistPage from './components/Checklist/CreateChecklistPage';
-import ChecklistMainContent from './components/Checklist/Checklist_Main';
-import ModifyDefaultChecklist from './components/Checklist/ModifyDefaultChecklist';
-import { SubmissionComponent } from './components/Checklist/DeptChecklistOptions';
-import { DocumentationComponent, ExternalHullPreBoardingComponent, ISMCodeComponent, RadioComponent, WheelHouseComponent, CommonCheckComponent, DeckHoursofRestComponent } from './components/Checklist/DeptChecklistOptions';
-import { DeckComponent, EngineComponent, SafetyComponent, LogisticsComponent, HospitalityComponent } from './components/Checklist/DeptChecklistOptions';
-import { ShoreSafety, DamageControlSafety, FireSafety, SurvivalSafety, IGSSafety, CrudeOilSafety, LiquiedChemicalsSafety, LiquefiedGasSafety } from './components/Checklist/DeptChecklistOptions';
-import { EngineRoom } from './components/Checklist/DeptChecklistOptions';
-import { ContainerLogistics } from './components/Checklist/DeptChecklistOptions';
-import { AccomodationHospitality } from './components/Checklist/DeptChecklistOptions';
+import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/checklistmaincontent" element={<ChecklistMainContent />} />
-          <Route path="/create-checklist" element={<CreateChecklistPage />} />
-          <Route path="/modify-default-checklist" element={<ModifyDefaultChecklist />} />
-          <Route path="/deck" element={<DeckComponent />} />
-          <Route path="/engine" element={<EngineComponent />} />
-          <Route path="/safety" element={<SafetyComponent />} />
-          <Route path="/logistics" element={<LogisticsComponent />} />
-          <Route path="/hospitality" element={<HospitalityComponent />} />
-          <Route path="/documentation" element={<DocumentationComponent />} />
-          <Route path="/external-hull-pre-boarding" element={<ExternalHullPreBoardingComponent />} />
-          <Route path="/ism-code" element={<ISMCodeComponent />} />
-          <Route path="/radio" element={<RadioComponent />} />
-          <Route path="/wheel-house" element={<WheelHouseComponent />} />
-          <Route path="/common-check" element={<CommonCheckComponent />} />
-          <Route path="/deck-hours-rest-fatigue" element={<DeckHoursofRestComponent />} />
-          <Route path="/engineroomcheck" element={<EngineRoom />} />
-          <Route path="/shore" element={<ShoreSafety />} />
-          <Route path="/damagecontrol" element={<DamageControlSafety />} />
-          <Route path="/fireprotect" element={<FireSafety />} />
-          <Route path="/survival" element={<SurvivalSafety />} />
-          <Route path="/igs" element={<IGSSafety />} />
-          <Route path="/crude-oil" element={<CrudeOilSafety />} />
-          <Route path="/liquid-chemicals" element={<LiquiedChemicalsSafety />} />
-          <Route path="/liquefied-gases" element={<LiquefiedGasSafety />} />
-          <Route path="/containerlogistics" element={<ContainerLogistics />} />
-          <Route path='/accomodationhospitality' element={<AccomodationHospitality />} />
-          <Route path='/submission' element={<SubmissionComponent />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
