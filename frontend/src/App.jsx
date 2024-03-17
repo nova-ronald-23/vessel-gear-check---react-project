66a241461365ea7b30202cda7b82c8000ddbbd98
Pavalan <139787075+Pavalansiva@users.noreply.github.com>
2024-03-17 23:25:39 +0530

    switch to multiply

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/Login'; 
-import SignUp from './components/SignUp';
-import ForgotPassword from './components/ForgotPassword';
-import Verification from './components/Verification';
-import NewPassword from './components/NewPassword';
 import './App.css';
-import WeatherApp from './components/WeatherApp/WeatherApp';
-import DashboardPage from './components/dashboard';
-import VesselProfile from './components/VesselProfile';
-import VesselProfileNext from './components/VesselProfileNext';
+import Createnew from './component/Createnew';
+/*import Select from './component/Select';*/
+/*import Selectone from './component/Selectone';*/
+/*import Selecttwo from './component/Selecttwo';*/
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<WeatherApp />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/forgotpassword" element={<ForgotPassword />} />
-        <Route path="/verification" element={<Verification />} />
-        <Route path="/newpassword" element={<NewPassword />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/vesselprofile" element={<VesselProfile />} />
-        <Route path="/VesselProfileNext" element={<VesselProfileNext />} />
-        {/* Other routes */}
-      </Routes>
-    </Router>
+    <Createnew />
   );
 }
 
